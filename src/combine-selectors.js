cc60135d2140319506116acb85874fa26f1f0858
Baz <baz@thinkloop.com>
2016-10-01 01:39:02 +0000

    invoke getState in getter

--- src/combine-selectors.js
+++ src/combine-selectors.js
@@ -1,7 +1,7 @@
-module.exports = function(selectors, ...args) {
+module.exports = function(selectors, getState, ...args) {
 	return Object.keys(selectors).reduce((p, selectorKey) => {
 		Object.defineProperty(p, selectorKey, {
-			get: function() { return selectors[selectorKey](...args) },
+			get: function() { return selectors[selectorKey](getState(), ...args) },
 			enumerable: true
 		});
 		return p;
