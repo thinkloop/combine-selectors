a3378414b850d94d6f2c6362e099fc5eebc60a5f
Baz <baz@thinkloop.com>
2016-10-01 01:05:45 +0000

    pass all arguments to getter

--- src/combine-selectors.js
+++ src/combine-selectors.js
@@ -1,7 +1,7 @@
-module.exports = function(selectors, getState) {
+module.exports = function(selectors, ...args) {
 	return Object.keys(selectors).reduce((p, selectorKey) => {
 		Object.defineProperty(p, selectorKey, {
-			get: function() { return selectors[selectorKey](getState()) },
+			get: function() { return selectors[selectorKey](...args) },
 			enumerable: true
 		});
 		return p;
